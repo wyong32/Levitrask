a32f7534ca2b9cd65b63a268b3bfd7b46f80d8da
WH <710569885@qq.com>
2025-04-19 19:04:46 +0800

    添加google

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import VueGtag from 'vue-gtag'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +11,16 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(
+  VueGtag,
+  {
+    config: {
+      id: 'G-N6VVHRSS1J',
+    },
+    router,
+  },
+  (gtag) => {},
+)
 
 app.mount('#app')
