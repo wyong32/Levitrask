5aa588037fd5f1d8c60ff15e1f95971ead010b62
WH <710569885@qq.com>
2025-04-19 19:12:20 +0800

    回退

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import VueGtag from 'vue-gtag'
 
 import App from './App.vue'
 import router from './router'
@@ -11,16 +10,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
-app.use(
-  VueGtag,
-  {
-    config: {
-      id: 'G-N6VVHRSS1J',
-    },
-    router,
-  },
-  (gtag) => {},
-)
 
 app.mount('#app')
